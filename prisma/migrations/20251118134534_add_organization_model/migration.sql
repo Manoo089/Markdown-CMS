27f9c9f94d30796d48f7aa18763e0e4fb7f0746c
Manoo089 <manoo089@googlemail.com>
2025-11-18 15:02:36 +0100

    feat: adjust prisma schema and migration for organization

---
+++ prisma/migrations/20251118134534_add_organization_model/migration.sql
@@ -0,0 +1,36 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[organizationId,slug]` on the table `Post` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `organizationId` to the `Post` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `organizationId` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Post" ADD COLUMN     "organizationId" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "organizationId" TEXT NOT NULL;
+
+-- CreateTable
+CREATE TABLE "Organization" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Organization_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Organization_slug_key" ON "Organization"("slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Post_organizationId_slug_key" ON "Post"("organizationId", "slug");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "Organization"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "Organization"("id") ON DELETE CASCADE ON UPDATE CASCADE;
