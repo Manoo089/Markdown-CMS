8a7a554f67c44178af0666b12f5f839e14f2f362
Manoo089 <manoo089@googlemail.com>
2025-11-23 17:11:52 +0100

    feat: add next-theme provider

--- providers.tsx
+++ providers.tsx
@@ -1,7 +1,22 @@
 "use client";
 
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { SessionProvider } from "next-auth/react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <NextAuthProvider>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        {children}
+      </ThemeProvider>
+    </NextAuthProvider>
+  );
+}
+
+function NextAuthProvider({ children }: { children: React.ReactNode }) {
   return <SessionProvider>{children}</SessionProvider>;
+}
+
+function ThemeProvider({ children, ...props }: React.ComponentProps<typeof NextThemesProvider>) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
